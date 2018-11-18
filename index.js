6e8a95e79aecf7b9a5d5e5d3f5df51da68a30dc6
fanfan <1025537699@163.com>
2018-11-18 08:31:46 +0800

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 (function() {
     'use strict';
     GM_addStyle(`#zizhuIcon {width: 36px; height: 36px;box-shadow: 2px 2px 4px rgba(12,12,12,.3); border-radius: 5px; cursor: pointer;}
-                 #zizhuMain {position: absolute; top: 0; left: 0; width: 0; height: 0; padding: 2px; background: #fff; font-size: 12px; opacity: 0;}`)
+                 #zizhuMain {position: absolute; top: 0; left: 0; width: 0; height: 0; padding: 2px; background: #fff; font-size: 12px; opacity: 0;z-index: 9999;}`)
     var innerIcon = "<img src='https://cdn-img.easyicon.net/png/376/37643.gif' id='zizhuIcon' alt='蜘蛛磁力搜索' title='蜘蛛磁力搜索'/>"
     var txt = ''
     var odom = document.createElement("div");
